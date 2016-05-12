c42a34b6f81472f958927fdbd59102a83a221ce2
Gaseema Ndung'u <gaseema.n@gmail.com>
2016-05-12 09:28:36 +0300

    added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -38,3 +38,17 @@ $(document).ready(function () {
     var interval = setInterval(updateClock, 1000);
 
 }); //EOF DOCUMENT.READY
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
